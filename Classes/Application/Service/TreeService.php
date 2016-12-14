f32e52551ee79044db9d377f198bba7d01dbf4bd
Bernhard Schmitt <b.schmitt@core4.de>
2016-12-14 11:26:21 +0100

    TASK: Add tree service for content context translation

---
+++ Classes/Application/Service/TreeService.php
@@ -0,0 +1,31 @@
+<?php
+namespace Nezaniel\Arboretum\ContentRepositoryAdaptor\Application\Service;
+
+/*
+ * This file is part of the Nezaniel.Arboretum.ContentRepositoryAdaptor package.
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\TYPO3CR\Domain\Service\Context as ContentContext;
+
+/**
+ * The Tree service
+ */
+class TreeService
+{
+    /**
+     * @param ContentContext $contentContext
+     * @return array
+     */
+    public function translateContentContextToTreeIdentity(ContentContext $contentContext)
+    {
+        $treeIdentity = [
+            'workspace' => $contentContext->getWorkspaceName(),
+        ];
+        foreach ($contentContext->getTargetDimensionValues() as $dimensionName => $contextDimensionValues) {
+            $treeIdentity['dimensionValues'][$dimensionName] = reset($contextDimensionValues);
+        }
+
+        return $treeIdentity;
+    }
+}
